23503bc30143fc2d0a0508cd0d2cd64ce33ee4ac
galucadasi <61360734+galucadasi@users.noreply.github.com>
2022-10-26 12:30:02 +0000

    Reset the whole code for mocno.

--- main.py
+++ main.py
@@ -9,42 +9,8 @@ o:
 import re
 from PyPDF2 import PdfFileReader
 
-#declaring variáveis 
-k=40
-#diff is change
-list_diff1 = []
-list_diff2 = []
-#Ganbiarra 
-rating1 = 1500
-rating2 = 1500
-
-#Lists
-player_list_rating = []
-list_players = []
-list_player1_res = []
-list_player2_res = []
-
-#regex config:
 REGEX_TABLE = r'(?P<table>\d+) (?P<player1>[\w ]+) (\d+\.\d) (?P<result>1-0|0-1|0.5-0.5) (\d+\.\d) (?P<player2>[\w ]+)'
- 
-#Interacao com o usuario
-def main_menu():
-  #recolhe o numero de usuarios
-  num_players = int(input('Número de jogadores:'))
-  # rating = int(input(''))
-  return num_players
 
-#Recolhe o rating does jogadores
-def get_rating_player(num_players,player_list_rating, list_players):
-  count_num_players = num_players
-  while count_num_players > 0:
-    player_rating = input('Digite o rating do jogador:', list_players[count_num_players])
-    player_list_rating.append(player_rating )
-    count_num_players -= 1
-  
-  print(player_list_rating)
-  return player_list_rating
-#Pegar texto do pdf 
 def extract_information(pdf_path):
     file_content = ''
     with open(pdf_path, 'rb') as f:
@@ -55,81 +21,57 @@ def extract_information(pdf_path):
             file_content += current_page.extractText()
     return file_content
 
-#Pega os resultados de todas as mesas de uma rodada
-def get_results(pdf_text, num_players):
-  pdf_lines = pdf_text.split('\n')
-  lines=0
+def get_results(pdf_text):
+  games = {}
 
-  #Adicionar o sistema que compreende o que é uma linha valida
-  pdf_lines_1char = pdf_lines[num_players].split()
-  print (pdf_lines_1char)
-  """
-  if isinstance(pdf_lines_1char, int):
-    print('Is integer')
-  """
-  while lines < num_players:
-#add an if to check if it is the first round to get the players
-    match = re.match(REGEX_TABLE, pdf_lines[7])
-    
+  for pdf_line in pdf_text.split('\n'):
+    match = re.match(REGEX_TABLE, pdf_line)
+
+    if match is None:
+      continue
+
     player1= match.group('player1')
     player2= match.group('player2')
     result = match.group('result')
-    if result == '1-0':
-      player1_res = 1
-      player2_res = 0
-    elif result == '0-1':
-      player1_res = 0
-      player2_res = 1
-    elif result == '0.5-0.5':
-      player1_res = 0.5
-      player2_res = 0.5
-    else:
-      player1_res = 10
-      player2_res = 10
-      print('Not found result')
-      lines =+1
-    list_player1_res.append(player1_res)
-    list_player2_res.append(player2_res)    
+    player1_res, player2_res = result.split('-')
+    player1_res, player2_res = float(player1_res), float(player2_res)
 
-#list_players.append(player1,player2)
-    return list_player1_res, list_player2_res, list_players
+    if player1 not in games:
+      games[player1] = []
 
-#Recebe o resultado e os ratings é devolve a variação 
-"""
-def calc_rating(rating1, rating2, player1_res, player2_res):
-  player1_diff = k*(player1_res[0]-(rating1/(rating1+rating2)))
-  player2_diff = k*(player2_res[0]-(rating2/(rating1+rating2)))
-  print(player1_diff, player2_diff)
-  return player1_diff, player2_diff
-"""
-# Função principal
-def main():
-  #diz o caminho do arquivo / automatizar isso dps
-  path = './axs30.pdf'
-  #Chama a função que extrai o texto do pdf
+    games[player1].append({
+      "opponent": player2,
+      "result": player1_res
+    })
+
+    if player2 not in games:
+      games[player2] = []
+
+    games[player2].append({
+      "opponent": player1,
+      "result": player2_res
+    })
+
+  return games
+
+def get_inputs(games):
+  ratings = {}
+
+  for name in games:
+    rating = int(input(f"Diga me o rating do jogador {name}"))
+
+    ratings[player1] = rating
+
+  return ratings
+
+def main(path):
   pdf_text = extract_information(path)
-  #print(pdf_text)
-  
-  #Chama a função que recebe as informações base do usuario
-  num_players = main_menu()
-  #Isso realmente converte para uma int?
-  num_players = int(num_players)
-  #Chama a função que pega o resultado da rodada 
-  list_player1_res, list_player2_res, list_players  = get_results(pdf_text,num_players)
-  
-#Chama a função que recolhe o rating dos jogadores
-  get_rating_player(num_players,player_list_rating, list_players)
 
-#Chama a função que calcula o diff
-#player1_diff, player2_diff = calc_rating(rating1, rating2, list_player1_res, list_player2_res)
+  print(pdf_text)
+  
+  games = get_results(pdf_text)
 
-""""
-def get_rating_player(num_players,player_list_rating):
-  player1_diff, player2_diff= calc_rating(rating1, rating2, player1_res, player2_res)
-  list_diff1.append(player1_diff)
-  list_diff2.append(player2_diff)
-  print(list_diff1, list_diff2)
-"""
+  ratings = get_inputs(games)
 
 if __name__ == '__main__':
-  main()
+  main('./axs30.pdf')

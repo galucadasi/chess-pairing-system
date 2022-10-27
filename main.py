04f8503b3f3cc0dfedd54db2be3dafa7a6c36790
galucadasi <61360734+galucadasi@users.noreply.github.com>
2022-10-27 11:59:09 +0000

    Finish

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ o:
 
 import re
 from PyPDF2 import PdfFileReader
+import math
 
 REGEX_TABLE = r'(?P<table>\d+) (?P<player1>[\w ]+) (\d+\.\d) (?P<result>1-0|0-1|0.5-0.5) (\d+\.\d) (?P<player2>[\w ]+)'
+
+K = 40
 
 def extract_information(pdf_path):
     file_content = ''
@@ -58,20 +61,49 @@ def get_inputs(games):
   ratings = {}
 
   for name in games:
-    rating = int(input(f"Diga me o rating do jogador {name}"))
+    rating = int(input(f"Diga me o rating do jogador {name}: "))
 
-    ratings[player1] = rating
+    ratings[name] = rating
 
   return ratings
 
+def probability(rating1, rating2):
+  if rating1 - rating2 > 400:
+    return 0
+  elif rating1 - rating2 < - 400:
+    return 1
+
+  return 1 / (1 + math.pow(10,(rating1 - rating2) / 400))
+ 
+ 
+def calc_rating(games, ratings):
+  rating_final = {}
+
+  for name, games in games.items():
+    rating1 = ratings[name]
+    delta_rating = 0
+
+    print('jogador:', name)
+
+    for game in games:
+      rating2 = ratings[game["opponent"]]
+      result = game["result"]
+
+      delta_rating += round((result - probability(rating2, rating1)) * K,  1)
+
+      print('probability(', rating2, rating1, ') #', probability(rating2, rating1), '->', result)
+      print(delta_rating)
+    rating_final[name] = rating1 + delta_rating
+  return rating_final
+
 def main(path):
   pdf_text = extract_information(path)
-
-  print(pdf_text)
   
   games = get_results(pdf_text)
 
   ratings = get_inputs(games)
-
+  
+  rating_final = calc_rating(games, ratings)
+  print(rating_final)
 if __name__ == '__main__':
-  main('./axs30.pdf')
+  main('./axs31.pdf')
